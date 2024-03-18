0f01391374a4464c5d19cf7b6e26281579ed090a
sebastiangarcia12323 <sg0793483@gmail.com>
2024-03-17 19:44:11 -0500

    trabajo

---
+++ formulario3.php
@@ -0,0 +1,17 @@
+<form action="cargo.php" target="trabajo">
+
+  
+
+ <p>Ingresa tu nombre completo: <input type="text" name="nombrecompleto"></p>
+
+  <p>
+
+    Deja un mensaje:<br>
+
+    <textarea name="mensaje"></textarea>
+
+  </p>
+
+  <input type="submit" value="Enviar la información">
+
+</form>
